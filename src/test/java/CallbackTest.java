4412cbbe2c6c225e06d4f514bf30736384b701fc
Альфия Устименко <alfiya.ustimenko@ukr.net>
2022-07-07 16:26:58 +0500

    Тест

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,19 +1,29 @@
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
+
 public class CallbackTest {
+
+    @BeforeEach
+    public void setup() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void shouldSubmitRequest() {
-        open("http://localhost:9999/");
-        SelenideElement from = $("[data-test-id=callback-form");
-        from.$("data-test-id=name] input").setValue("Василий");
-        from.$("data-test-id=phone] input").setValue("+79678765645");
-        from.$("data-test-id=agreement]").click();
-        from.$("data-test-id=button]").click();
-        $(".order-success").shouldHave(exactText("Ваша заявка успешно отправлена!Наш менеджер свяжется с вамив ближайшее время."));
+
+        $("[data-test-id=name] input").setValue("Василий Васильев");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+
+        $("[data-test-id=order-success]").shouldHave(
+                exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
